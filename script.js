68c6c1ebcb9879628d58f6ee6cffa0bea5b2330b
Marcus_Gu <109802075+gyb13912193900@users.noreply.github.com>
2024-02-07 23:09:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 function updateCounter() {
-    const startDate = new Date('2022-08-09');
+    const startDate = new Date('2022-08-09T00:00:00');
     const now = new Date();
+    const diff = now - startDate;
 
-    const totalSeconds = Math.floor((now - startDate) / 1000);
-    const days = Math.floor(totalSeconds / (3600 * 24));
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const minutes = Math.floor(totalSeconds / 60) % 60;
-    const seconds = totalSeconds % 60;
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diff / 1000 / 60) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
 
     const formattedTime = `${days} Days : ${hours} Hours : ${minutes} Minutes : ${seconds} Seconds`;
     document.getElementById('daysCounter').innerText = formattedTime;
 }
 
-// Update the count every second
 setInterval(updateCounter, 1000);
-
-document.addEventListener('DOMContentLoaded', updateCounter);
